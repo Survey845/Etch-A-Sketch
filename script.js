d5e0e15605776b15ad7ce4fabb10ee78d5cf3c06
Survey845 <bhanjaimukund@gmail.com>
2024-09-06 22:22:10 +0530

    Made the RGB randomizer and darkness increment on hovering

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ dimensionButton.classList.add("dimension");
 
 dimensionButton.addEventListener("click" ,() => {
     let pixelNumber = prompt("What should be the dimension of the pixel grid?: ");
-    if(Number(pixelNumber)<=100){
+    if(Number(pixelNumber)<=100 && Number(pixelNumber)>0){
         dimensionButton.textContent = pixelNumber + "x" + pixelNumber;
         pixelNumber = Number(pixelNumber);
         makeGrid(pixelNumber);
@@ -20,17 +20,29 @@ function makeGrid(num=16){
                 let gridSquare = document.createElement("div");
                 container.appendChild(gridSquare);
                 gridSquare.classList.add("square");
-                gridSquare.style.height = `${squareSize}px`;
+                gridSquare.style.minHeight = `${squareSize}px`;
                 gridSquare.style.width = `${squareSize}px`;
+                gridSquare.style.opacity = "0";
+                let bgColor = getRandomColor();
                 gridSquare.addEventListener("mouseover", () => {
-                    gridSquare.style.backgroundColor = "black";
-                })
-                gridSquare.addEventListener("mouseout", () => {
-                    gridSquare.style.backgroundColor = "white";
+                    gridSquare.style.backgroundColor = bgColor;
+                    let currentOpacity = parseFloat(gridSquare.style.opacity);
+                    if(currentOpacity<1){
+                        gridSquare.style.opacity = (currentOpacity+0.1).toFixed(1);
+                    }
                 })
             }
 }
 makeGrid();
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 
 
